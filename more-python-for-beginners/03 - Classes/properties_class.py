28cf6d53935047298c84bfbebdb735ed8097b267
SCY <seochaeyeon01@gmail.com>
2020-05-30 17:06:02 +0900

    [UPDATE]add coment

--- more-python-for-beginners/03 - Classes/properties_class.py
+++ more-python-for-beginners/03 - Classes/properties_class.py
@@ -3,16 +3,16 @@ class Presenter():
 		# Constructor
 		self.name = name
 
-	@property
+	@property       #getter function
 	def name(self):
 		print('Retrieving name...')
 		return self.__name
-	@name.setter
+	@name.setter        #setter function
 	def name(self, value):
 		# cool validation here
 		print('Validating name...')
 		self.__name = value
 
-presenter = Presenter('Chris')
+presenter = Presenter('Chris') 
 presenter.name = 'Christopher'
 print(presenter.name)
