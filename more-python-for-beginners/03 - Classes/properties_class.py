87fcdccb86f312388d1dc320198fcacf9b7f7236
SCY <seochaeyeon01@gmail.com>
2020-06-01 23:39:32 +0900

    [UPDATE]rearrange comments

--- more-python-for-beginners/03 - Classes/properties_class.py
+++ more-python-for-beginners/03 - Classes/properties_class.py
@@ -1,20 +1,20 @@
 class Presenter():
-	def __init__(self, name):
-		# Constructor
-		self.name = name
+    def __init__(self, name):
+        # Constructor
+        self.name = name
         
         #getter function
         @property
-	def name(self):
-		print('Retrieving name...')
-		return self.__name
-
-	#setter function
+        def name(self):
+            print('Retrieving name...')
+            return self.__name
+        
+        #setter function
         @name.setter
-	def name(self, value):
-		# cool validation here
-		print('Validating name...')
-		self.__name = value
+        def name(self, value):
+            # cool validation here
+            print('Validating name...')
+            self.__name = value
 
 presenter = Presenter('Chris') 
 presenter.name = 'Christopher'
