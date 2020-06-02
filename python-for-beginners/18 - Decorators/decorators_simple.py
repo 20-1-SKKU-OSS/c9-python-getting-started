ace2083dec37cb29617490c285cc8235cf2c2f89
SCY <seochaeyeon01@gmail.com>
2020-06-02 23:13:58 +0900

    [ADD]add new code

--- python-for-beginners/18 - Decorators/decorators_simple.py
+++ python-for-beginners/18 - Decorators/decorators_simple.py
@@ -1,12 +1,20 @@
 def decorator_function(original_function):
-    def wrapper_function():  
-        return original_function()  
-    return wrapper_function  
+    def wrapper_function():
+        print '{} function is not called yet.'.format(original_function.__name__)
+        return original_function()
+    return wrapper_function
 
 
-def display():
-    print 'display function is running.'
+@decorator_function
+def display_1():
+    print 'display_1 function is running.'
 
-decorated_display = decorator_function(display) 
 
-decorated_display()
+@decorator_function
+def display_2():
+    print 'display_2 function is funning.'
+
+
+display_1()
+print
+display_2()
