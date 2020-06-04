bc02ab9cf91d9bc93823e0f49258a2ed47c39ca5
SCY <seochaeyeon01@gmail.com>
2020-06-04 22:00:09 +0900

    [UPDATE]update code

--- python-for-beginners/18 - Decorators/decorators_simple.py
+++ python-for-beginners/18 - Decorators/decorators_simple.py
@@ -5,7 +5,6 @@ def decorator_function(original_function):
     return wrapper_function
 
 
-@decorator_function
 def display_1():
     print ('display_1 function is running.')
 
@@ -14,6 +13,8 @@ def display_1():
 def display_2():
     print ('display_2 function is running.')
 
+
+display_1 = decorator_function(display_1)
 
 display_1()
 print()
