36cc8390dd7e4984f6f8ff929c7e29f91e404dc2
SCY <seochaeyeon01@gmail.com>
2020-06-04 21:56:02 +0900

    [FIX]fix errors

--- python-for-beginners/18 - Decorators/decorators_simple.py
+++ python-for-beginners/18 - Decorators/decorators_simple.py
@@ -1,20 +1,20 @@
 def decorator_function(original_function):
     def wrapper_function():
-        print '{} function is not called yet.'.format(original_function.__name__)
+        print ('{} function is not called yet.'.format(original_function.__name__))
         return original_function()
     return wrapper_function
 
 
 @decorator_function
 def display_1():
-    print 'display_1 function is running.'
+    print ('display_1 function is running.')
 
 
 @decorator_function
 def display_2():
-    print 'display_2 function is funning.'
+    print ('display_2 function is running.')
 
 
 display_1()
-print
+print()
 display_2()
