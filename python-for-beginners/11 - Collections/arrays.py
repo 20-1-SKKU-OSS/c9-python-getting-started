649381f857709effdc728c2ac8542aad1c0023c9
SCY <seochaeyeon01@gmail.com>
2020-06-04 22:14:17 +0900

    [UPDATE]fix coments

--- python-for-beginners/11 - Collections/arrays.py
+++ python-for-beginners/11 - Collections/arrays.py
@@ -1,12 +1,12 @@
 from array import array
 
-#scores = ['d']
+#scores = ('d')
 scores = array('d')
 
-#scores = ['d', 97]
+#scores = ('d', [97])
 scores.append(97)
 
-#scores = ['d', 97, 98]
+#scores = ('d', [97, 98])
 scores.append(98)
 
 print(scores)
