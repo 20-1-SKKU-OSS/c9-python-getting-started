0483c71b96831e3981f5b28909566b4dd102cc4b
Kang SungMin <50102066+devquint@users.noreply.github.com>
2020-06-05 11:33:56 +0900

    [UPDATE] commenting convention
    same as above, one space between # and comment

--- python-for-beginners/11 - Collections/dictionaries.py
+++ python-for-beginners/11 - Collections/dictionaries.py
@@ -1,6 +1,6 @@
 person = {'first': 'Christopher'}
 
-#person = {'last': 'Harrison', 'first': 'Christopher'}
+# person = {'last': 'Harrison', 'first': 'Christopher'}
 person['last'] = 'Harrison'
 
 print(person)
