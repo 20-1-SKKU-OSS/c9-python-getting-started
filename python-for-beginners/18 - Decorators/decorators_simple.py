618bbd7a2292ace9541c72db29bceeda7d0efd3e
Kang SungMin <50102066+devquint@users.noreply.github.com>
2020-06-05 11:30:29 +0900

    [UPDATE] removed one space between print and ()

--- python-for-beginners/18 - Decorators/decorators_simple.py
+++ python-for-beginners/18 - Decorators/decorators_simple.py
@@ -1,17 +1,17 @@
 def decorator_function(original_function):
     def wrapper_function():
-        print ('{} function is not called yet.'.format(original_function.__name__))
+        print('{} function is not called yet.'.format(original_function.__name__))
         return original_function()
     return wrapper_function
 
 
 def display_1():
-    print ('display_1 function is running.')
+    print('display_1 function is running.')
 
 
 @decorator_function
 def display_2():
-    print ('display_2 function is running.')
+    print('display_2 function is running.')
 
 
 display_1 = decorator_function(display_1)
