67e05352ec08c0c3a6a01260f0d32ad2109b9168
Hyuntae Jeong <63701384+Hyuntae-Jeong@users.noreply.github.com>
2020-06-04 22:20:43 -0700

    [Update] Update dictionaries.py
    i've changed comment  # person = {'last': 'Harrison', 'first': 'Christopher'} into # person = {'first': 'Christopher', 'last': 'Harrison'} in previous commit. But i've restored it since I'm not sure about the order.
    Please check the order that I'm confused about.

--- python-for-beginners/11 - Collections/dictionaries.py
+++ python-for-beginners/11 - Collections/dictionaries.py
@@ -1,7 +1,7 @@
 person = {'first': 'Christopher'}
 
 person['last'] = 'Harrison'
-# person = {'first': 'Christopher', 'last': 'Harrison'}
+# person = {'last': 'Harrison', 'first': 'Christopher'}
 
 print(person)
 print(person['first'])
