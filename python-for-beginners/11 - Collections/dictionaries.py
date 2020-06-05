452941ef12ada63b543479fe3ad89196bd6bbd89
SCY <seochaeyeon01@gmail.com>
2020-06-05 18:54:27 +0900

    [UPDATE]update comments

--- python-for-beginners/11 - Collections/dictionaries.py
+++ python-for-beginners/11 - Collections/dictionaries.py
@@ -1,7 +1,8 @@
 person = {'first': 'Christopher'}
 
-person['last'] = 'Harrison'
 # person = {'last': 'Harrison', 'first': 'Christopher'}
 # python dictionary is unorderd. So we can also recognized it as person = {'first': 'Christopher', 'last': 'Harrison'}
+person['last'] = 'Harrison'
+
 print(person)
 print(person['first'])
