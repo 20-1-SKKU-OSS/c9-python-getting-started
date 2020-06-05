d18dba5a28485b229e9b3268169be15b020b33f1
Hyuntae Jeong <63701384+Hyuntae-Jeong@users.noreply.github.com>
2020-06-04 22:28:48 -0700

    [Update] Added additional comment about dictionary
    added description of python dictionary that the orders don't matter

--- python-for-beginners/11 - Collections/dictionaries.py
+++ python-for-beginners/11 - Collections/dictionaries.py
@@ -2,6 +2,6 @@ person = {'first': 'Christopher'}
 
 person['last'] = 'Harrison'
 # person = {'last': 'Harrison', 'first': 'Christopher'}
-
+# python dictionary is unorderd. So we can also recognized it as person = {'first': 'Christopher', 'last': 'Harrison'}
 print(person)
 print(person['first'])
