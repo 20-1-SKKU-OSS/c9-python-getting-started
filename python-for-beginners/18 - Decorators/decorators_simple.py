c2d5c54178ac12950e5e5461c8fffb290f05edd1
Devquint <ksmin1114@g.skku.edu>
2020-06-06 14:44:36 +0900

    [UPDATE] implemented one more case
    display_1 uses function call and reassign to make decorator function, display_2 uses direct function call that using decorator function, display_3 use @ annotation to use decorator function.

--- python-for-beginners/18 - Decorators/decorators_simple.py
+++ python-for-beginners/18 - Decorators/decorators_simple.py
@@ -1,7 +1,8 @@
 def decorator_function(original_function):
     def wrapper_function():
-        print('{} function is not called yet.'.format(original_function.__name__))
+        print('{} function is about to be called'.format(original_function.__name__))
         return original_function()
+
     return wrapper_function
 
 
@@ -9,13 +10,24 @@ def display_1():
     print('display_1 function is running.')
 
 
-@decorator_function
 def display_2():
     print('display_2 function is running.')
+
+
+@decorator_function
+def display_3():
+    print('display_3 function is running.')
 
 
 display_1 = decorator_function(display_1)
 
 display_1()
 print()
+
+decorator_function(display_2)()
+print()
+
 display_2()
+print()
+
+display_3()
