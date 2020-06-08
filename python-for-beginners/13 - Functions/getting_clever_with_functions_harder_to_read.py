3b3cc81963112984d430696d33384912e4ae45ee
jesusleejy <jesusleejy@naver.com>
2020-06-09 00:18:54 +0900

    [FIX]fix a typing error/getting_clever_with_functions_harder_to_read.py

--- python-for-beginners/13 - Functions/getting_clever_with_functions_harder_to_read.py
+++ python-for-beginners/13 - Functions/getting_clever_with_functions_harder_to_read.py
@@ -8,7 +8,7 @@ def get_initial(name):
     initial = name[0:1].upper()
     return initial
 
-#Ask for someone's name and return the initials
+# Ask for someone's name and return the initials
 first_name = input('Enter your first name: ')
 middle_name = input('Enter your middle name: ')
 last_name = input('Enter your last name: ')
